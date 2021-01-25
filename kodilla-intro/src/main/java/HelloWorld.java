8405f46abefc4c337173780ec29cb7ddfd166aa9
Paweł Molek <pmolek@02.pl>
2021-01-25 20:54:04 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Pawel");
+        System.out.println("32 lata");
+        System.out.println("M");
+    }
+}
---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Pawel";
+        int myAge = 32;
+        double height = 1.79;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String firstClass = "Hello from FirstClass";
+
+        System.out.println(myName);
+        System.out.println(firstClass);
+    }
+}
