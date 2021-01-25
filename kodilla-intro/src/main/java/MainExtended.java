c8ec9df5669c6566c927295a2562a29bd949f057
Paweł Molek <pmolek@02.pl>
2021-01-25 21:04:57 +0100

    MainExtended

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class MainExtended {
+public class MainExtended {
+    public static void main(String [] args){
+        String myName = "Pawel";
+        String mySurname = "Molek";
+        int myAge = 32;
+        double height = 1.79;
+        char gender = 'M';
+        boolean tester = true;
+
+        System.out.println("Moje imie to");
+        System.out.println(myName);
+        System.out.println("Moje nazwisko to");
+        System.out.println(mySurname);
+        System.out.println("Moj wiek to");
+        System.out.println(myAge);
+        System.out.println("Moj wzrost to");
+        System.out.println(height);
+        System.out.println("Moja plec to");
+        System.out.println(gender);
+        System.out.println("Moj cel szkolenia to zostać testerem");
+        System.out.println(tester);
+
+    }
 }
