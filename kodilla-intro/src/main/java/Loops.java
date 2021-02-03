30c2f11e51a685c0ad787d64f96fa700a4a4af88
Paweł <pmolek@o2.pl>
2021-02-04 00:45:12 +0100

    0.12 i 0.13

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -23,16 +23,5 @@ public class Loops {
 
 
     }
-    public int getCountOfRandomNumber(int max) {
-        Random random = new Random();
-        int result = 0;
-        int sum = 0;
-        while (sum < max) {
-            int temp = random.nextInt(10);
-            sum = sum + temp;
-            result++;
-        }
-        return result;
-        System.out.println(random);
-    }
+
 }
