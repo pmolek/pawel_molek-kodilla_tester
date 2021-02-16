ea5c8b728c7c8851d9f5caf47ca84b2f97827bf4
Paweł <pmolek@o2.pl>
2021-02-16 15:08:35 +0100

    2.3

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,22 @@
 public class FirstClass {
-    public static void main(String[] args){
-        System.out.println("Hello from FirstClass!");
+
+    public static void main (String[] args){
+        Notebook notebook = new Notebook(600,1000,2010);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYearAndPrice();
+
+        Notebook heavyNotebook = new Notebook(2000,1500,2019);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYearAndPrice();
+
+        Notebook oldNotebook = new Notebook(1600,500,2000);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYearAndPrice();
     }
 }
