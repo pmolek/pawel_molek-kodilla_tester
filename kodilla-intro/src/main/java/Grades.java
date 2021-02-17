b8dc55cc5930f12ed97b348417c74feaa8672a54
Paweł <pmolek@o2.pl>
2021-02-17 14:52:22 +0100

    2.3

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Grades {
+
+    private int[] grades;
+    private int size;
+
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+}
+
+
---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,17 @@
+public class User {
+   String name;
+   int age;
+
+
+   public static void main(String[] args){
+       User pawel = new User("Pawel", 32);
+       User piotr = new User("Piotr", 25);
+       User jan = new User("jan", 18);
+       User maciej = new User("Maciej", 37);
+       User kuba = new User("Kuba", 42);}
+       User[] users = {pawel, piotr, jan, maciej, kuba};
+
+
+   }
+
+}
