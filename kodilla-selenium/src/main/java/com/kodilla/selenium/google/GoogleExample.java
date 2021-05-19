635895f18f69c2f5c48bbd351e3610a0aaa7b366
Paweł <pmolek@o2.pl>
2021-05-20 01:48:22 +0200

    20.05

--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleExample {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
 
